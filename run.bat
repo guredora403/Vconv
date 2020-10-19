ca32831e370a99af4a1173695cb84b96b5f5c94f
guredora <contact@guredora.com>
2020-10-19 18:25:44 +0900

    uninstall.batの追加と半角スペースを含むファイル名に対応

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @cd /d %~dp0
-@data\ffmpeg -i data\black.jpg -i %~f1 -acodec copy -g 150 -qcomp 0.7 -qmin 10 -qmax 51 -qdiff 4 -subq 6 -me_range 16 -i_qfactor 0.714286 %~dpn1.mp4
+@data\ffmpeg -i data\black.jpg -i "%~f1" -acodec copy -g 150 -qcomp 0.7 -qmin 10 -qmax 51 -qdiff 4 -subq 6 -me_range 16 -i_qfactor 0.714286 "%~dpn1.mp4"
 @pause
---
+++ uninstall.bat
@@ -0,0 +1,3 @@
+del /q %appdata%\Microsoft\Windows\SendTo\動画変換.lnk
+echo アンインストール完了
+pause
