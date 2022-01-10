34c47fd67d016fd48a9071d7920b70dab95b06de
guredora <contact@guredora.com>
2022-01-10 15:10:11 +0900

    コーデックを修正

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @cd /d %~dp0
-@data\ffmpeg -i data\black.jpg -i "%~f1" -acodec copy -g 150 -qcomp 0.7 -qmin 10 -qmax 51 -qdiff 4 -subq 6 -me_range 16 -i_qfactor 0.714286 "%~dpn1.mp4"
+@data\ffmpeg -i data\black.jpg -i "%~f1" -movflags +faststart -c:v libx264 -profile:v high -level:v 4.0 -b_strategy 2 -bf 2 -flags cgop -coder ac -pix_fmt yuv420p -crf 23 -maxrate 1M -bufsize 2M -c:a aac -ac 2 -ar 48000 -b:a 384k "%~dpn1.mp4"
 @pause
